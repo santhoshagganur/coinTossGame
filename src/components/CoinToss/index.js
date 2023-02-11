d504d8d84e0227294c2670ccc6002a690e0b56a7
santhoshagganur <agganursanthosh@gmail.com>
2023-02-11 18:20:03 +0000

    passed

--- src/components/CoinToss/index.js
+++ src/components/CoinToss/index.js
@@ -3,7 +3,12 @@ import {Component} from 'react'
 import './index.css'
 
 class CoinToss extends Component {
-  state = {total: 0, heads: 0, tails: 0}
+  state = {
+    total: 0,
+    heads: 0,
+    tails: 0,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/heads-img.png',
+  }
 
   tossTheCoin = () => {
     const tossResult = Math.floor(Math.random() * 2)
@@ -12,28 +17,26 @@ class CoinToss extends Component {
       this.setState(prevState => ({
         total: prevState.total + 1,
         heads: prevState.heads + 1,
+        imageUrl: 'https://assets.ccbp.in/frontend/react-js/heads-img.png',
       }))
     } else {
       this.setState(prevState => ({
         total: prevState.total + 1,
         tails: prevState.tails + 1,
+        imageUrl: 'https://assets.ccbp.in/frontend/react-js/tails-img.png',
       }))
     }
   }
 
   render() {
-    const {total, heads, tails} = this.state
+    const {total, heads, tails, imageUrl} = this.state
 
     return (
       <div className="bg-container">
         <div className="card-container">
           <h1 className="heading"> Coin Toss Game </h1>
           <p className="decision"> Heads (or) Tails </p>
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/heads-img.png"
-            className="coin-logo"
-            alt="coin"
-          />
+          <img src={imageUrl} className="coin-logo" alt="toss result" />
           <button className="button" type="button" onClick={this.tossTheCoin}>
             Toss Coin
           </button>
